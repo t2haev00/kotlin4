e297c6ff1fe1c36edf5eeb0682b6992cb3c4a75a
t2haev00 <eveliinahampus@gmail.com>
2024-02-09 00:07:46 +0200

    First part: view

--- app/src/main/java/com/example/theming/MainActivity.kt
+++ app/src/main/java/com/example/theming/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.theming
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.theming.ui.theme.ThemingTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,17 +38,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    val appModifier = Modifier.fillMaxWidth().padding(8.dp)
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        ){
+        Text(
+            text = "My title",
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = appModifier
+        )
+        OutlinedTextField(
+            value = "", 
+            onValueChange = { /* TODO*/ } ,
+            modifier = appModifier
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = appModifier
+        ) {
+            Text(text = "Submit")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ThemingTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
